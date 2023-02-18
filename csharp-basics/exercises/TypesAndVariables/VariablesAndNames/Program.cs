e43373150b6dae6c1983fa55d6b8b9285e3ed0d6
zambala <redzambala@gmx.es>
2023-02-18 17:14:46 +0200

    finished 1

--- csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
@@ -13,18 +13,25 @@ namespace VariablesAndNames
             int carsDriven;
 
             double seatsInCar;
+            double carpoolCapacity;
+            double averagePassengersPerCar;
 
             cars = 100;
             drivers = 28;
             passengers = 90;
             seatsInCar = 4.00;
-            carsNotDriven = cars - (cars / drivers);
-            carsDriven = cars / drivers;
+
+            carsNotDriven = cars - drivers;
+            carsDriven = cars - carsNotDriven;
+            carpoolCapacity = carsDriven * (seatsInCar - 1);
+            averagePassengersPerCar = carpoolCapacity / carsDriven;
 
             Console.WriteLine("There are " + cars + " cars available.");
             Console.WriteLine("There are only " + drivers + " drivers available.");
             Console.WriteLine("There will be " + carsNotDriven + " empty cars today.");
+            Console.WriteLine("We can transport " + carpoolCapacity + " people today.");
             Console.WriteLine("We have " + passengers + " to carpool today.");
+            Console.WriteLine("We need to put about " + averagePassengersPerCar + " in each car.");
             Console.ReadKey();
         }
     }
