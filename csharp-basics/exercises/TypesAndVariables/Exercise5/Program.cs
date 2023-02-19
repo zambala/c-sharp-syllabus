cca540ec98d75d1c68c8127c4913a76a7adac5c0
zambala <redzambala@gmx.es>
2023-02-19 21:15:33 +0200

    1, 2, 3, 4, 5, 7, 8 Exercises finished

--- csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
@@ -6,10 +6,39 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
+            
+            var teachJavascript = "Andis";
+            var classJavascript = "Javascript";
 
-            Console.WriteLine("What is your Name?");
+            var teachBigBoss = "Sandris";
+            var classBigBoss = "Discord";
 
-            String.Format("|{0,5}|{1,5}|{2,5}|{3,5}|", "arg0", "arg1", "arg2", "arg3");
+            var teachCSharp = "Edgars";
+            var classCSharp = "C#";
+
+            var teachGit = "Līva";
+            var classGit = "Github";
+
+            var teachAdmin = "Baiba";
+            var classAdmin = "Admin";
+
+            int[] idNr = {1, 2, 3, 4, 5};
+
+
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -7 } {3, -7 }", "+------------", "-----------------------------------+", "", "");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -20 } {3, 6 }{4, 8 }", "|", "ID", "Class", "Teacher", "|");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, 7 }", "|", "-----------------------------------", "|");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -20 } {3, 6 }{4, 9 }", "|", idNr[0], classJavascript, teachJavascript, "|");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -20 } {3, 6 }{4, 8 }", "|", idNr[1], classBigBoss, teachBigBoss, "|");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -20 } {3, 6 }{4, 9 }", "|", idNr[2], classCSharp, teachCSharp, "|");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -20 } {3, 6 }{4, 9 }", "|", idNr[3], classGit, teachGit, "|");
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -20 } {3, 6 }{4, 9 }", "|", idNr[4], classAdmin, teachAdmin, "|");
+
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -7 } {3, -7 }{4, 21 }", "|", "", "", "", "|");
+
+            Console.WriteLine("{0, -6 }{1, -6 } {2, -7 } {3, -7 }{4, -7 }", "+------------", "-----------------------------------+", "", "", "");
+
+            Console.ReadKey();
 
         }
     }
