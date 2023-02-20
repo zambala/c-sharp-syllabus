6e83ff4fdaa2100439cc45b1a9952622fb5a31b1
zambala <redzambala@gmx.es>
2023-02-20 03:56:15 +0200

    semi final push

--- csharp-basics/exercises/TypesAndVariables/Exercise9/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise9/Program.cs
@@ -6,7 +6,31 @@ namespace Exercise9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Input distance(metres): ");
+            decimal distance = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Input Time (in minutes): ");
+            int timeMinutes = int.Parse(Console.ReadLine());
+
+            int timeSeconds = timeMinutes * 60;
+            double timeHoursSimple = (double)timeMinutes / 60;
+
+            var timeHours = TimeSpan.FromSeconds(timeSeconds);
+            Console.WriteLine("{0:00}:{1:00}:{2:00}", (int)timeHours.TotalHours, timeHours.Minutes, timeHours.Seconds);
+
+            decimal metersSecond = distance / timeSeconds;
+            decimal KilometersHour = (distance / 1000) / ((decimal)timeSeconds / 3600);
+            decimal milesHour = KilometersHour / (decimal)1.609;
+
+          
+            Console.WriteLine($"Your speed in metres/sec is {metersSecond}");
+            Console.WriteLine($"Your speed in km/h is {KilometersHour}");
+            Console.WriteLine($"Your speed in miles/h is {milesHour}");
+            
+            Console.WriteLine($"distance is {distance} Meters and it took {timeMinutes} Minutes, {timeSeconds} seconds or {timeHours} hours or {timeHoursSimple}, speed {metersSecond} m/s, {KilometersHour} km/h or {milesHour} mph");
+
+            Console.ReadKey();
         }
     }
 }
