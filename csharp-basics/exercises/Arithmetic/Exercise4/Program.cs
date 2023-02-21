fbb19306e8d0ad409028f42e5303dff3e5d9ec3f
zambala <redzambala@gmx.es>
2023-02-21 18:07:10 +0200

    1-4 exercises finihed

--- csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace Exercise4
+namespace Product1ToN
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int product;
+            int i;
+
+            
+            product = 1;
+
+            for (i = 1; i <= 10; i++)
+            {
+                
+                product = product * i;
+            }
+
+            Console.WriteLine("Product is " + product);
+            Console.ReadKey();
         }
     }
 }
