d36416cca9e52d5968fe287d6a8d1391560fe7b9
zambala <redzambala@gmx.es>
2023-02-21 02:48:41 +0200

    1-2 exercises finished

--- csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -6,7 +6,20 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 != 0)
+            {
+                Console.WriteLine("Odd Number");
+            }
+            else
+            {
+                Console.WriteLine("Even Number");
+                
+            }
+            Console.WriteLine("Bye!");
+
         }
     }
 }
