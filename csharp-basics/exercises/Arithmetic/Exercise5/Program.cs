3789155010d4c84192f80007bbcb27e5784c72c8
zambala <redzambala@gmx.es>
2023-02-21 19:24:22 +0200

    1-5 exercises finished

--- csharp-basics/exercises/Arithmetic/Exercise5/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise5/Program.cs
@@ -6,7 +6,32 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int min = 1;
+            int max = 100;
+
+            Random random = new Random();
+            int number = random.Next(min, max);
+
+            //Console.WriteLine(number);
+
+            Console.WriteLine("I'm thinking of a number between 1-100.  Try to guess it.");
+
+            int guess1 = int.Parse(Console.ReadLine());
+
+            if (guess1 < number)
+            {
+                Console.WriteLine($"Sorry, you are too low.  I was thinking of {number}.");
+            }
+            else if (guess1 > number)
+            {
+                Console.WriteLine($"Sorry, you are too high.  I was thinking of  {number}.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!  What are the odds?!?");
+            }
+
         }
     }
 }
