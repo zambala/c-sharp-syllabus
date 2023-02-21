0d5b93f0af250a0d04877b27f81e520217cb6a3f
zambala <redzambala@gmx.es>
2023-02-22 01:39:07 +0200

    1-5 done, 6 with problems

--- csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -6,7 +6,43 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0;
+
+            //for (var i = 1; i < 101; i++)
+            //{
+            //    if (i % 3 == 0) Console.WriteLine("Coza");
+            //    else if (i % 5 == 0) Console.WriteLine("Loza");
+            //    else if (i % 7 == 0) Console.WriteLine("Woza");
+            //    else Console.WriteLine(i);
+            //}
+
+            for (int i = 1; i <= 110; i++)
+            {
+
+
+                if (i % 3 == 0) Console.WriteLine("Coza");
+                else if (i % 5 == 0) Console.WriteLine("Loza");
+                else if (i % 7 == 0) Console.WriteLine("Woza");
+                //else Console.WriteLine(i);
+
+
+                if ((i % 11) == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.Write($"{i},");
+                }
+
+                //if (i % 3 == 0)
+                //{
+                //    //Console.WriteLine("Coza");
+
+                //    string s = i.ToString("Coza");
+                //    Console.WriteLine(s);
+                //}
+            }
         }
     }
 }
