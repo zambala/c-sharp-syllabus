9f13f1325a00d4c1c48e03b6a1585e57f9484886
zambala <redzambala@gmx.es>
2023-02-23 13:45:47 +0200

    1-7, 9, 11 finished, 8, 10 still thinking

--- csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -8,14 +8,19 @@ namespace Exercise6
         {
             for (var i = 1; i <= 110; i++)
             {
-                if (i % 3 == 0) Console.WriteLine("Coza");
-                else if (i % 5 == 0) Console.WriteLine("Loza");
-                else if (i % 7 == 0) Console.WriteLine("Woza");
-                else Console.WriteLine(i);
+                int count = 0;
+                if (i % 3 == 0) Console.Write("Coza ");
+                else if (i % 5 == 0) Console.Write("Loza ");
+                else if (i % 7 == 0) Console.Write("Woza ");
+                else Console.Write(i + " ");
+
+               
+                if (i % 11 == 0)
+                    Console.WriteLine("\n");
             }
 
-            
-            
+
+
         }
     }
 }
