6b92aa51277308179d815fb99c75ab9df049e906
zambala <redzambala@gmx.es>
2023-02-23 11:50:54 +0200

    1-5, 7, 9, 11 Finished, 6 with minor problems; 8, 10 still thinking; 2vers

--- csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -6,43 +6,17 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-            int n = 0;
 
-            //for (var i = 1; i < 101; i++)
-            //{
-            //    if (i % 3 == 0) Console.WriteLine("Coza");
-            //    else if (i % 5 == 0) Console.WriteLine("Loza");
-            //    else if (i % 7 == 0) Console.WriteLine("Woza");
-            //    else Console.WriteLine(i);
-            //}
-
-            for (int i = 1; i <= 110; i++)
+            for (var i = 1; i <= 110; i++)
             {
-
-
-                //if (i % 3 == 0) Console.WriteLine("Coza");
-                //else if (i % 5 == 0) Console.WriteLine("Loza");
-                //else if (i % 7 == 0) Console.WriteLine("Woza");
-                //else Console.WriteLine(i);
-
-
-                if ((i % 11) == 0)
-                {
-                    Console.WriteLine(i);
-                }
-                else
-                {
-                    Console.Write($"{i},");
-                }
-
-                //if (i % 3 == 0)
-                //{
-                //    //Console.WriteLine("Coza");
-
-                //    string s = i.ToString("Coza");
-                //    Console.WriteLine(s);
-                //}
+                if (i % 3 == 0) Console.WriteLine("Coza");
+                else if (i % 5 == 0) Console.WriteLine("Loza");
+                else if (i % 7 == 0) Console.WriteLine("Woza");
+                else Console.WriteLine(i);
             }
+
+            
+            
         }
     }
 }
