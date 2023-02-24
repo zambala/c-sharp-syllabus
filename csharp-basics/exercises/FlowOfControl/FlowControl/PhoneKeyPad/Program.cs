bb7f5d835b9ab5e587faca6b48e32c80e18071ff
zambala <redzambala@gmx.es>
2023-02-24 23:38:20 +0200

    1,2,4,6 exercises finished

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PhoneKeyPad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneKeyPad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -10,70 +10,70 @@ namespace TenBillion
             Console.WriteLine("Input an integer number less than ten billion: ");
 
             var input = Console.ReadLine();
+            int n = int.Parse(input);
 
-            /*
-            todo - check if number
-            if (?) 
-            {
-                //todo - check if n is less than zero
-                if (?) 
+            //todo - check if number
+            if ()
                 {
-                    n *= -1;
-                }
-
-                fixme
-                if (?) 
-                {
-                    Console.WriteLine("Number is greater or equals 10,000,000,000!");
-                } 
-                else 
-                {
-                    int digits = 1;
-                    if (?) 
+                    //todo - check if n is less than zero
+                if (input < 0)
                     {
-                        digits = 2;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 3;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 4;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 5;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 6;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 7;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 8;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 9;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 10;
+                        n *= -1;
                     }
 
-                    Console.WriteLine("Number of digits in the number: " + digits);
+                    //fixme
+                if (input >= 1000000000)
+                    {
+                        Console.WriteLine("Number is greater or equals 10,000,000,000!");
+                    }
+                    else
+                    {
+                        int digits = 1;
+                        if (?)
+                        {
+                            digits = 2;
+                        }
+                        else if (?)
+                        {
+                            digits = 3;
+                        }
+                        else if (?)
+                        {
+                            digits = 4;
+                        }
+                        else if (?)
+                        {
+                            digits = 5;
+                        }
+                        else if (?)
+                        {
+                            digits = 6;
+                        }
+                        else if (?)
+                        {
+                            digits = 7;
+                        }
+                        else if (?)
+                        {
+                            digits = 8;
+                        }
+                        else if (?)
+                        {
+                            digits = 9;
+                        }
+                        else if (?)
+                        {
+                            digits = 10;
+                        }
+
+                        Console.WriteLine("Number of digits in the number: " + digits);
+                    }
                 }
-            } 
-            else 
-            {
-                Console.WriteLine("The number is not a long");
-            }
-            */
+                else
+                {
+                    Console.WriteLine("The number is not a long");
+                }
+
         }
     }
 }
