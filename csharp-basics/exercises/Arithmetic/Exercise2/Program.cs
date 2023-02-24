84e85fc188f324f23fa46dce0f24a1adf7e0d445
zambala <redzambala@gmx.es>
2023-02-24 12:16:58 +0200

    all finished except 8

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 != 0)
+            {
+                Console.WriteLine("Odd Number");
+            }
+            else
+            {
+                Console.WriteLine("Even Number");
+            }
+            Console.WriteLine("Bye!");
+        }
+    }
+}
--- csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
+++ csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
@@ -2,19 +2,24 @@
 
 namespace SumAverageRunningInt
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             var sum = 0;
-            int average;
+            double average;
             const int lowerBound = 1;
             const int upperBound = 100;
 
-            for (var number = lowerBound; number <= upperBound; ++number) 
+            for (var number = lowerBound; number <= upperBound; number++)
             {
                 sum += number;
             }
+            average = (double)sum / 100;
+
+            Console.WriteLine($"The Sum of number from 1 to 100 is {sum}");
+            Console.WriteLine($"The average is {average}");
+            Console.ReadKey();
         }
     }
 }
