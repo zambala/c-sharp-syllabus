669397c6ac53f9be38343bb7db728e9f37d00c67
zambala <redzambala@gmx.es>
2023-02-25 03:22:30 +0200

    finished for now....

--- csharp-basics/exercises/FlowOfControl/FlowControl/PhoneKeyPad/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PhoneKeyPad/Program.cs
@@ -6,7 +6,35 @@ namespace PhoneKeyPad
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter some text");
+            string text = Console.ReadLine().ToUpper();
+
+            string[] str
+            = { "2",    "22",  "222", "3",   "33", "333",
+                "4",    "44",  "444", "5",   "55", "555",
+                "6",    "66",  "666", "7",   "77", "777",
+                "7777", "8",   "88",  "888", "9",  "99",
+                "999",  "9999" };
+
+            Console.WriteLine(phoneKeys(str, text));
+        }
+        static String phoneKeys(string[] arr, string text)
+        {
+            string result = "";
+
+            int n = text.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (text[i] == ' ')
+                    result = result + "0";
+
+                else
+                {
+                    int position = text[i] - 'A';
+                    result = result + arr[position];
+                }
+            }
+            return result;
         }
     }
 }
--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -4,68 +4,62 @@ namespace TenBillion
 {
     class Program
     {
-        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
 
             var input = Console.ReadLine();
-            int n = int.Parse(input);
+            Int64 n = int.Parse(input);
 
-            //todo - check if number
-            if ()
+            if (n == int.Parse(input))
                 {
-                    //todo - check if n is less than zero
-                if (input < 0)
+                if (n < 0)
                     {
                         n *= -1;
                     }
-
-                    //fixme
-                if (input >= 1000000000)
+                if (n >= 10000000000)
                     {
                         Console.WriteLine("Number is greater or equals 10,000,000,000!");
                     }
                     else
                     {
                         int digits = 1;
-                        if (?)
+                        if (n >= 10 && n <= 99)
                         {
                             digits = 2;
                         }
-                        else if (?)
+                        else if (n >= 100 && n <= 999)
                         {
                             digits = 3;
                         }
-                        else if (?)
+                        else if (n >= 1000 && n <= 9999)
                         {
                             digits = 4;
                         }
-                        else if (?)
+                        else if (n >= 10000 && n <= 99999)
                         {
                             digits = 5;
                         }
-                        else if (?)
+                        else if (n >= 100000 && n <= 999999)
                         {
                             digits = 6;
                         }
-                        else if (?)
+                        else if (n >= 1000000 && n <= 9999999)
                         {
                             digits = 7;
                         }
-                        else if (?)
+                        else if (n >= 10000000 && n <= 99999999)
                         {
                             digits = 8;
                         }
-                        else if (?)
+                        else if (n >= 100000000 && n <= 999999999)
                         {
                             digits = 9;
                         }
-                        else if (?)
+                        else if (n == 1000000000)
                         {
                             digits = 10;
                         }
-
                         Console.WriteLine("Number of digits in the number: " + digits);
                     }
                 }
@@ -73,7 +67,6 @@ namespace TenBillion
                 {
                     Console.WriteLine("The number is not a long");
                 }
-
         }
     }
 }
