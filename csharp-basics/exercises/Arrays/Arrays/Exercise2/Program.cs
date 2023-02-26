cab31bf538f12a54d336c768936496d6b4a2272a
zambala <redzambala@gmx.es>
2023-02-26 15:39:32 +0200

    1, 2, 11 finished

--- csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
@@ -1,34 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise2
 {
     class Program
     {
-        // TODO: Write a C# program to sum values of an array.
-        // Array values are starting from min (including) till max (including) number. 
         private static void Main(string[] args)
         {
             var sum = 0;
+
             Console.WriteLine("Please enter a min number");
             int minNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter a max number");
             int maxNumber = int.Parse(Console.ReadLine());
 
-            //int[] inputArr = new int {int.minNumber, maxNumber };
+            int[] minMax = Enumerable.Range(minNumber, maxNumber - minNumber + 1).ToArray();
+            Console.WriteLine("New Array is " + string.Join(",", minMax));
 
-
-
-            //fixme
-            for (int i = minNumber; i <= maxNumber; i++)
+            for (int i = 0; i < minMax.Length; i++)
             {
-                sum += inputArr[i];
-
-                
+                sum += minMax[i];
             }
 
             Console.WriteLine("The sum is " + sum);
-
-
             Console.ReadKey();
         }
     }
