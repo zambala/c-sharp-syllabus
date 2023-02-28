f42186fcae53469665a222695a9c68d1fdbf5445
zambala <redzambala@gmx.es>
2023-02-28 05:54:42 +0200

    1-6, 11 finished. 7-10 thinking

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] input = { 92, 6, 73, -77, 81, -90, 99, 8, -85, 34, };
+            CountPosSumNeg([92, 6, 73, -77, 81, -90, 99, 8, -85, 34]);
+        }
+
+        public int[] CountPosSumNeg(int[] input)
+        {
+            int length = input.Length;
+            int[] arraySecond = new int[2];
+            var sumPos = 0;
+            var sumNeg = 0;
+
+            for (int i = 0; i < length; i++) 
+            {
+                if (input[i] > 0)
+                {
+                    sumPos += input[i];
+                }
+                else
+                {
+                    sumNeg += input[i];
+                }
+                return arraySecond;
+            }
+            return arraySecond;
+        }
+    }
+}
---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayFirst = { "samuel", "MABELLE", "letitia", "meridith" };
+
+        }
+
+
+
+        public static string CapMe(string[] arrayFirst)
+        {
+            
+            int length = arrayFirst.Length;
+            string[] arraySecond = new string[length];
+
+
+            foreach (string word in arrayFirst)
+            {
+                Console.WriteLine(word[0]);
+            }
+            return arraySecond[arrayFirst[]];
+
+        }
+        }
+}
