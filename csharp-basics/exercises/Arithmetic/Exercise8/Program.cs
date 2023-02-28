fed44d55524f2ea228843541472f560b7a13d861
zambala <redzambala@gmx.es>
2023-02-28 16:29:59 +0200

    All Finished, Arithmetic

--- csharp-basics/exercises/Arithmetic/Exercise8/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise8/Program.cs
@@ -6,7 +6,39 @@ namespace Exercise8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("employee 1: ");
+            CalculatePay(7.50, 35);
+            Console.Write("employee 2: ");
+            CalculatePay(8.20, 47);
+            Console.Write("employee 3: ");
+            CalculatePay(10.00, 73);
+            Console.ReadKey();
+        }
+
+        public static double CalculatePay(double pay, double hours)
+        {
+            double minimumWage = 8.00;
+            double maxHours = 60;
+            double totalSalary = 0;
+
+            if (pay < minimumWage || hours > maxHours)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                if (hours > 40)
+                {
+                    totalSalary = pay * 40 + 1.5 * pay * (hours - 40);
+                }
+                else
+                {
+                    totalSalary = pay * hours;
+                }
+                Console.WriteLine("Your total salary is " + totalSalary);
+            }
+
+            return totalSalary;
         }
     }
 }
