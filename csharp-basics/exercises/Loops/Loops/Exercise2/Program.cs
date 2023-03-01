b7bad6d47d6d2794e2373dc18e0648674c1188c6
zambala <redzambala@gmx.es>
2023-03-01 13:35:01 +0200

    1-2 finished

--- csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
@@ -10,14 +10,12 @@ namespace Exercise2
             
             Console.WriteLine("Input number of terms : ");
             n = Convert.ToInt32(Console.ReadLine());
-            /*
-            todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.Pow()
-            */
-            for (i = 0; i < n; i++)
+
+            for (i = 1; i <= n; i++)
             {
+                Console.WriteLine(i*i);
             }
 
-            Console.ReadKey();
         }
     }
 }
