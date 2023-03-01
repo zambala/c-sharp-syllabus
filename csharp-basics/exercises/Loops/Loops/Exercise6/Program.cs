3ab4f639df34d484687c539d1b659928beb4632a
zambala <redzambala@gmx.es>
2023-03-01 16:40:39 +0200

    1-6 finished

--- csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
@@ -6,7 +6,32 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please, Enter a Number!");
+            int number = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i <= number; i++)
+            {
+                if(i % 15 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % 15 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+                if(i % 20 == 0)
+                {
+                    Console.Write("\n");
+                }
+            }
         }
     }
 }
