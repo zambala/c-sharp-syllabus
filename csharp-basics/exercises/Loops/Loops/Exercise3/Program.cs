15b2d22806d799bb33f235d5d761577df7ba49df
zambala <redzambala@gmx.es>
2023-03-01 14:29:53 +0200

    1-3 finished

--- csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
@@ -6,7 +6,27 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] arrayRandom = new int[20];
+            Random rand = new Random();
+
+            for (int i = 0; i < arrayRandom.Length; i++)
+            {
+                arrayRandom[i] = rand.Next(1, 20);
+            }
+            Console.Write($"Random numbers are: " + string.Join(",", arrayRandom) + "\n");
+
+            Console.WriteLine("Enter the position out of 20 random numbers you want to know");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput <= 0 || userInput > 20)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine($"You have chosen a number {arrayRandom[userInput -1]}");
+            }
         }
     }
 }
