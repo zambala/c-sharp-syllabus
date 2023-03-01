3a5f2a0fbc9f1022f4cbb6b5870c704c6077c0f8
zambala <redzambala@gmx.es>
2023-03-01 23:47:43 +0200

    1-7 finished

--- csharp-basics/exercises/Loops/Loops/Exercise7/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise7/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace Exercise7
+namespace Piglet
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int points = 0;
+            bool continueGame = true;
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to Piglet!");
+            while (continueGame)
+            {
+                int randomNumber = random.Next(1, 7);
+                Console.WriteLine($"You rolled a {randomNumber}");
+
+                if (randomNumber == 1)
+                {
+                    continueGame = false;
+                    points = 0;
+                    Console.WriteLine($"Game Over :You got {points} points.");
+                }
+                else
+                {
+                    points += randomNumber;
+                    Console.WriteLine($"You got {points} points.");
+                    Console.Write("Roll again (y/n)?:");
+
+                    if (Console.ReadLine() == "y")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Game Over :You got {points} points.");
+                        break;
+                    }
+                }
+            }
         }
     }
 }
