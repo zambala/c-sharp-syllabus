76cfb998bcef3cd5d2a64e95f95c5befae84711e
zambala <redzambala@gmx.es>
2023-03-01 16:10:18 +0200

    1-5 finished

--- csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -6,7 +6,22 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a word: ");
+            string wordFirst = Console.ReadLine();
+            Console.WriteLine("Enter another word: ");
+            string wordSecond = Console.ReadLine();
+
+            int wordsTogether = (wordFirst + wordSecond).Length;
+
+            Console.Write(wordFirst);
+
+            for (int i = 0; i < 30 - wordsTogether; i++)
+            {
+                Console.Write(".");
+            }
+
+            Console.Write(wordSecond);
+            Console.WriteLine("\n");
         }
     }
 }
