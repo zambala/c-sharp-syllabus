f5f464d95ecbd5e82db67b2466e117e15547b8eb
zambala <redzambala@gmx.es>
2023-03-02 02:01:48 +0200

    1-10 finished

--- csharp-basics/exercises/Loops/Loops/Exercise10/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise10/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Exercise10
+namespace NumberSquare
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter min number");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter max number");
+            int max = int.Parse(Console.ReadLine());
+
+            for (int i = min; i <= max; i++)
+            {
+                for (int j = i; j <= max; j++)
+                {
+                    Console.Write(j);
+                }
+                for (int j = min; j < i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
