606968cd6c40084fa11d1a4af3ccf67194e3d156
zambala <redzambala@gmx.es>
2023-03-02 02:43:57 +0200

    Loppds finished for now

--- csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
@@ -6,7 +6,31 @@ namespace Exercise11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str1 = "Happy Birthday";
+            string str2 = "MANY THANKS";
+            string str3 = "sPoNtAnEoUs";
+
+            ReverseCase(str1);
+            ReverseCase(str2);
+            ReverseCase(str3);
+        }
+
+        static void ReverseCase(string sentence)
+        {
+            var charArray = sentence.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (char.IsUpper(charArray[i]))
+                {
+                    Console.Write(char.ToLower(charArray[i]));
+                }
+                else
+                {
+                    Console.Write(char.ToUpper(charArray[i]));
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
