0eaacc91464ea0d5b6e67d08bf1783845f6285ba
zambala <redzambala@gmx.es>
2023-03-05 11:05:49 +0200

    1-2 finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Product
+    {
+        public string name;
+        public double _priceAtStart;
+        public int amountAtStart;
+
+        public Product(string Name, double PriceAtStart, int AmountAtStart)
+        {
+            name = Name;
+            _priceAtStart = PriceAtStart;
+            amountAtStart = AmountAtStart;
+        }
+
+        public string PrintProduct()
+        {
+            return name + ", price " + String.Format("{0:0.00} EUR", _priceAtStart) + ", available " + amountAtStart + " items.";
+        }
+
+        public void ChangePrice(double newPrice)
+        {
+            _priceAtStart = newPrice;
+        }
+
+        public int ChangeSmallerCount(int newCount)
+        {
+            return amountAtStart -= newCount;
+        }
+
+        public int ChangeBiggerCount(int newCount)
+        {
+            return amountAtStart += newCount;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
