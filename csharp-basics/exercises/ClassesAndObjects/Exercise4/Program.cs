127a27028f1b210a7a3113630cd8f5cc0afa6f49
zambala <redzambala@gmx.es>
2023-03-05 17:49:40 +0200

    1-4 finished

--- csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -6,7 +6,25 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Movie[] movies = new Movie[3];
+            movies[0] = new Movie("Casino Royale", "Eon Productions", "PG13");
+            movies[1] = new Movie("Casino Glass", "Buena Vista International", "PG13");
+            movies[2] = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            foreach (Movie movie in movies)
+            {
+                Console.WriteLine($"Movie title: {movie.GetTitle()}, movie studio: {movie.GetStudio()}, movie rating:{movie.GetRating()}");
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Movies with rating \"PG\"");
+            Console.WriteLine("\n");
+
+            Movie[] pgMovies = Movie.GetPg(movies);
+            foreach (Movie movie in pgMovies)
+            {
+                Console.WriteLine($"Movie title: {movie.GetTitle()}, movie studio: {movie.GetStudio()}, movie rating:{movie.GetRating()}");
+            }
         }
     }
 }
