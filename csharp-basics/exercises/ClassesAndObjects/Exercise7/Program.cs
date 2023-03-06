5f8b8af36dfbb831bbfd98ad1e18557235e43317
zambala <redzambala@gmx.es>
2023-03-06 12:46:33 +0200

    1-7 finished

--- csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
@@ -6,7 +6,47 @@ namespace Exercise7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How much money is in the account?");
+            var balance = Convert.ToDecimal(Console.ReadLine());
+
+            if (balance < 0)
+            {
+                Console.WriteLine("Error try again");
+                balance = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter annual interest rate");
+            var interest = Convert.ToDecimal(Console.ReadLine());
+
+            if (interest < 0)
+            {
+                Console.WriteLine("Error try again");
+                interest = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            SavingsAccount acc1 = new SavingsAccount(balance, interest);
+
+            Console.WriteLine("How long has the account been opened?");
+            var monthsOpened = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= monthsOpened; i++)
+            {
+                Console.WriteLine($"Enter amount deposited for month {i}");
+                decimal depoAmount = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine($"Enter amount withdrawn for month {i}");
+                decimal WithrAmount = Convert.ToDecimal(Console.ReadLine());
+
+                acc1.Deposit(depoAmount);
+                acc1.Withdrawal(WithrAmount);
+                acc1.Interest(interest);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Total deposited: {Math.Round(acc1._totalDeposit, 2)}");
+            Console.WriteLine($"Total withdrawn: {Math.Round(acc1._totalWithdrawal, 2)}");
+            Console.WriteLine($"Interest earned: {Math.Round(acc1._totalInterest, 2)}");
+            Console.WriteLine($"Ending balance: {Math.Round(acc1._balance, 2)}");
+            Console.ReadKey();
         }
     }
 }
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/SavingsAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    internal class SavingsAccount
+    {
+        public decimal _interestRate;
+        public decimal _balance;
+        public decimal _totalDeposit;
+        public decimal _totalWithdrawal;
+        public decimal _totalInterest;
+
+        public SavingsAccount(decimal startingBalance, decimal interest)
+        {
+            _balance = startingBalance;
+            _interestRate = interest;
+        }
+
+        public void Withdrawal(decimal amount)
+        {
+            _balance -= amount;
+            _totalWithdrawal += amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+            _totalDeposit += amount;
+        }
+
+        public void Interest(decimal interest)
+        {
+            var monthlyInterest = _interestRate / 12 * _balance;
+
+            _totalInterest += monthlyInterest;
+            _balance = monthlyInterest + _balance;
+        }
+    }
+}
