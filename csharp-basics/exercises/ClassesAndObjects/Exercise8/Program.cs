04485ab54e68f1587ba29f53fe7496a95d304560
zambala <redzambala@gmx.es>
2023-03-06 14:03:22 +0200

    1-8 finished

--- csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -6,7 +6,19 @@ namespace Exercise8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+
+            Console.WriteLine("Before swap: \n");
+            Console.WriteLine("(" + p1._x + ", " + p1._y + ")");
+            Console.WriteLine("(" + p2._x + ", " + p2._y + ")");
+
+            Point.SwapPoints(ref p1, ref p2);
+
+            Console.WriteLine("\nAfter swap:\n");
+            Console.WriteLine("(" + p1._x + ", " + p1._y + ")");
+            Console.WriteLine("(" + p2._x + ", " + p2._y + ")");
+            Console.ReadKey();
         }
     }
 }
