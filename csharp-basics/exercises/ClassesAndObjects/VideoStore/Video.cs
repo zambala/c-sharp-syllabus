913a77f68760aae03ed50e67b8ae0e2dd627ff43
zambala <redzambala@gmx.es>
2023-03-06 16:17:14 +0200

    1-10 finished

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
@@ -1,44 +1,49 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace VideoStore
 {
     class Video
     {
+        public string _title;
+        public bool _flag;
+        public List<double> _rating;
         public Video(string title)
         {
-            
+            _title = title;
+            _rating = new List<double>();
         }
 
         public void BeingCheckedOut()
         {
-            
+            _flag = false;
         }
 
         public void BeingReturned()
         {
-            
+            _flag = true;
         }
 
         public void ReceivingRating(double rating)
         {
-            
+            _rating.Add(rating);
         }
 
         public double AverageRating()
         {
-            return 0;
+            return (_rating.Sum() / _rating.Count);
         }
 
         public bool Available()
         {
-            return true;
+            return _flag;
         }
 
-        public string Title => "";
+        public string Title => _title;
 
         public override string ToString()
         {
-            return $"{Title} {AverageRating()} {Available()}";
+            return $"Movie: {_title} with average rating: {AverageRating()} available: {Available()}";
         }
     }
 }
