7f2478d38c5dd83a5962e2ab5de7cc3b84321104
zambala <redzambala@gmx.es>
2023-03-06 15:07:55 +0200

    1-9 finished

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -10,6 +10,16 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            Account acc1 = new Account("Benson", (decimal)17.25);
+            Account acc2 = new Account("Benson", (decimal)-17.25);
+            Account acc3 = new Account("Benson", (decimal)0);
+            Account acc4 = new Account("Benson", (decimal)300);
+
+            Console.WriteLine(acc1.ShowUserNameAndBalance());
+            Console.WriteLine(acc2.ShowUserNameAndBalance());
+            Console.WriteLine(acc3.ShowUserNameAndBalance());
+            Console.WriteLine(acc4.ShowUserNameAndBalance());
+            Console.ReadKey();
         }
     }
 }
