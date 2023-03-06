b7c964d00ba2144df01a03f6e3ae60601d01e1c9
zambala <redzambala@gmx.es>
2023-03-06 09:49:36 +0200

    1-6 finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    internal class Dog
+    {
+        private string _name;
+        private bool _isMale;
+        public Dog Mother { private get; set; }
+        public Dog Father { private get; set; }
+
+        public Dog(string name, bool isMale, Dog mother = null, Dog father = null)
+        {
+            _name = name;
+            _isMale = isMale;
+            Mother = mother;
+            Father = father;
+        }
+
+        public string FathersName()
+        {
+            return Father == null ? "Unknown" : Father._name;
+        }
+
+        public bool HasSameMotherAs(Dog otherDog)
+        {
+            return Mother == otherDog.Mother ? true : false;
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Exercise6
 {
-    internal class Program
+    internal class DogTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dog dog01 = new Dog("Max", true);
+            Dog dog02 = new Dog("Rocky", true);
+            Dog dog03 = new Dog("Sparky", true);
+            Dog dog04 = new Dog("Buster", true);
+            Dog dog05 = new Dog("Sam", true);
+            Dog dog06 = new Dog("Lady", false);
+            Dog dog07 = new Dog("Molly", false);
+            Dog dog08 = new Dog("Coco", false);
+
+            dog01.Mother = dog06;
+            dog01.Father = dog02;
+            dog02.Mother = dog07;
+            dog02.Father = dog05;
+            dog04.Mother = dog06;
+            dog04.Father = dog03;
+            dog08.Mother = dog07;
+            dog08.Father = dog04;
+
+            Console.WriteLine(dog08.FathersName());
+            Console.WriteLine(dog03.FathersName());
+
+            Console.WriteLine(dog04.HasSameMotherAs(dog01));
         }
     }
 }
