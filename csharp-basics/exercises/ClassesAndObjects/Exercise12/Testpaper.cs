d3c1f6d7ad1a2606ddbe0d7cc8ba4ddc579b3d4f
zambala <redzambala@gmx.es>
2023-03-07 12:49:33 +0200

    1-12 finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise12/Testpaper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    public class TestPaper : ITestpaper
+    {
+        public string Subject
+        {
+            get;
+        }
+
+        public string[] MarkScheme
+        {
+            get;
+        }
+
+        public string PassMark
+        {
+            get;
+        }
+        public TestPaper(string subject, string[] markScheme, string passMark)
+        {
+            Subject = subject;
+            MarkScheme = markScheme;
+            PassMark = passMark;
+        }
+    }
+}
