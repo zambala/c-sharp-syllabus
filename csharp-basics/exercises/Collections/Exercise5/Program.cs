108bc56d47c95a0ba6e30330ffc7968386939d49
zambala <redzambala@gmx.es>
2023-03-09 12:03:51 +0200

    1-5 finished

--- csharp-basics/exercises/Collections/Exercise5/Program.cs
+++ csharp-basics/exercises/Collections/Exercise5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise5
 {
@@ -6,7 +7,70 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HappyNumbers();
+        }
+
+        public static void HappyNumbers()
+        {
+            while (true)
+            {
+                var numberList = new List<int>();
+                Console.WriteLine("Enter a number");
+                var userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int value))
+                {
+                    if (value < 0)
+                    {
+                        Console.WriteLine("Error! Positive numbers only");
+                    }
+                    else
+                    {
+                        numberList.Add(value);
+
+                    }
+
+                    while (value > 1)
+                    {
+                        value = CalculateSquare(value);
+                        Console.WriteLine(value);
+
+                        if (value == 1)
+                        {
+                            Console.WriteLine("Happy");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                        }
+                        else if (numberList.Contains(value))
+                        {
+                            Console.WriteLine("Not happy");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            numberList.Add(value);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a number...");
+                }
+            }
+        }
+
+        public static int CalculateSquare(int num)
+        {
+
+            var sum = 0;
+
+            while (num != 0)
+            {
+                sum += (num % 10) * (num % 10);
+                num /= 10;
+            }
+            return sum;
         }
     }
 }
