2642a0e23f953dc5b1047500f3adfd0c6b8384f6
zambala <redzambala@gmx.es>
2023-03-09 23:46:30 +0200

    1-10 finished

--- csharp-basics/exercises/Collections/Exercise10/Program.cs
+++ csharp-basics/exercises/Collections/Exercise10/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercise10
 {
@@ -6,7 +8,28 @@ namespace Exercise10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stringHashSet = new HashSet<string>();
+
+            stringHashSet.Add("aaaa");
+            stringHashSet.Add("bbbb");
+            stringHashSet.Add("cccc");
+            stringHashSet.Add("dddd");
+            stringHashSet.Add("eeee");
+
+            foreach (var str in stringHashSet)
+            {
+                Console.WriteLine(str);
+            }
+
+            stringHashSet.Clear();
+
+            Console.WriteLine(CheckForDuplicate(stringHashSet));
+            Console.ReadKey();
+        }
+
+        public static bool CheckForDuplicate(HashSet<string> set)
+        {
+            return set.Count != set.Distinct().Count();
         }
     }
 }
