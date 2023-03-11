7538ae2a4ea05f246dfb292e6cfddf4041af9fa0
zambala <redzambala@gmx.es>
2023-03-11 22:38:23 +0200

    1-, 7 finished

---
+++ csharp-basics/exercises/Polymorphism/MakeSounds/Parrot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeSounds
+{
+    public class Parrot : ISound
+    {
+        public void PlaySound()
+        {
+            Console.WriteLine("Krrrrrr");
+        }
+    }
+}
