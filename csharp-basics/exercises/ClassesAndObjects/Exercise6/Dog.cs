5631029e67cb6a78af0c1d6bdf0a9ee8f982f6e0
zambala <redzambala@gmx.es>
2023-03-14 03:27:00 +0200

    some recommendations fixed

--- csharp-basics/exercises/ClassesAndObjects/Exercise6/Dog.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Dog.cs
@@ -1,34 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercise6
+﻿namespace Exercise6
 {
-    internal class Dog
+    public class Dog
     {
-        private string _name;
-        private bool _isMale;
-        public Dog Mother { get; set; }
-        public Dog Father { get; set; }
+        public string _name;
+        private string _sex;
+        private string _mother;
+        private string _father;
 
-        public Dog(string name, bool isMale, Dog mother = null, Dog father = null)
+        public Dog(string name, string sex, string mother, string father)
         {
             _name = name;
-            _isMale = isMale;
-            Mother = mother;
-            Father = father;
+            _sex = sex;
+            _mother = mother;
+            _father = father;
+        }
+
+        public Dog(string name, string sex)
+        {
+            _name = name;
+            _sex = sex;
+            _mother = "Unknown";
+            _father = "Unknown";
         }
 
         public string FathersName()
         {
-            return Father == null ? "Unknown" : Father._name;
+            return _father;
         }
 
         public bool HasSameMotherAs(Dog otherDog)
         {
-            return Mother == otherDog.Mother ? true : false;
+            return _mother == otherDog._mother;
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
@@ -6,28 +6,19 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-            Dog dog01 = new Dog("Max", true);
-            Dog dog02 = new Dog("Rocky", true);
-            Dog dog03 = new Dog("Sparky", true);
-            Dog dog04 = new Dog("Buster", true);
-            Dog dog05 = new Dog("Sam", true);
-            Dog dog06 = new Dog("Lady", false);
-            Dog dog07 = new Dog("Molly", false);
-            Dog dog08 = new Dog("Coco", false);
+            Dog dog1 = new Dog("Max", "male", "Lady", "Rocky");
+            Dog dog2 = new Dog("Rocky", "male", "Molly", "Sam");
+            Dog dog3 = new Dog("Sparky", "male");
+            Dog dog4 = new Dog("Buster", "male", "Lady", "Sparky");
+            Dog dog5 = new Dog("Sam", "male");
+            Dog dog6 = new Dog("Lady", "female");
+            Dog dog7 = new Dog("Molly", "female");
+            Dog dog8 = new Dog("Coco", "female", "Molly", "Buster");
 
-            dog01.Mother = dog06;
-            dog01.Father = dog02;
-            dog02.Mother = dog07;
-            dog02.Father = dog05;
-            dog04.Mother = dog06;
-            dog04.Father = dog03;
-            dog08.Mother = dog07;
-            dog08.Father = dog04;
+            Console.WriteLine(dog8.FathersName());
+            Console.WriteLine(dog3.FathersName());
 
-            Console.WriteLine(dog08.FathersName());
-            Console.WriteLine(dog03.FathersName());
-
-            Console.WriteLine(dog04.HasSameMotherAs(dog01));
+            Console.WriteLine(dog4.HasSameMotherAs(dog1));
         }
     }
 }
